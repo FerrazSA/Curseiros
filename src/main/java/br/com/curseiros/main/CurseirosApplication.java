30f7b2d3428e1a2ad7b16b7be00ef46b38799a5e
Felipe Ferraz de Santana <felipeferrazs@outlook.com>
2025-12-18 23:46:57 -0300

    first commit

---
+++ src/main/java/br/com/curseiros/main/CurseirosApplication.java
@@ -0,0 +1,13 @@
+package br.com.curseiros.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseirosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurseirosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/curseiros/main/CurseirosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curseiros.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseirosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
