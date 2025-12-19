9efe6b225d369d5c75de0ffc315f8f67c777c872
Felipe Ferraz de Santana <felipeferrazs@outlook.com>
2025-12-19 13:49:31 -0300

    build: adicionado swagger no projeto para documentação

--- src/main/java/br/com/curseiros/main/CurseirosApplication.java
+++ src/main/java/br/com/curseiros/main/CurseirosApplication.java
@@ -1,9 +1,14 @@
 package br.com.curseiros.main;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Cursos",
+        description = "API responsável pela gestão de cursos, fazendo o CRUD dos dados no banco de dados",
+        version = "1.0"))
 public class CurseirosApplication {
 
     public static void main(String[] args) {
