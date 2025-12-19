7518654fdb89e53afdb5d88250d3f6246027c9b1
Felipe Ferraz de Santana <felipeferrazs@outlook.com>
2025-12-19 09:07:08 -0300

    feat: criado fluxo de listagem de todos os cursos do banco

--- src/main/java/br/com/curseiros/main/curso/controllers/CursoController.java
+++ src/main/java/br/com/curseiros/main/curso/controllers/CursoController.java
@@ -2,13 +2,11 @@ package br.com.curseiros.main.curso.controllers;
 
 import br.com.curseiros.main.curso.entities.CursoEntity;
 import br.com.curseiros.main.curso.useCases.CreateCursoUseCase;
+import br.com.curseiros.main.curso.useCases.ListCursoUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cursos")
@@ -16,6 +14,9 @@ public class CursoController {
 
     @Autowired
     private CreateCursoUseCase createCursoUseCase;
+
+    @Autowired
+    private ListCursoUseCase listCursoUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CursoEntity curso) {
@@ -28,4 +29,17 @@ public class CursoController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getAll() {
+        try {
+
+            var result = listCursoUseCase.execute();
+            return ResponseEntity.ok(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
---
+++ src/main/java/br/com/curseiros/main/curso/useCases/ListCursoUseCase.java
@@ -0,0 +1,20 @@
+package br.com.curseiros.main.curso.useCases;
+
+import br.com.curseiros.main.curso.entities.CursoEntity;
+import br.com.curseiros.main.curso.repositories.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ListCursoUseCase {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<CursoEntity> execute(){
+        List<CursoEntity> listCursos = cursoRepository.findAll();
+        return listCursos;
+    }
+}
